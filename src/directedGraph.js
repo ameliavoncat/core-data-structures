abcac46310897d7823ea87e7117892ac14c8a538
Amelia Clarke <ameliavoncat@gmail.com>
2017-01-24 10:29:04 -0800

    DirectedGraph fixup

--- src/directedGraph.js
+++ src/directedGraph.js
@@ -1,4 +1,4 @@
-class DirectedGraph {
+export default class DirectedGraph {
   constructor(){
     this.vertices = []
     this.directions = []
@@ -74,39 +74,42 @@ class DirectedGraph {
   findShortestPath(vertex1, vertex2){
     const vertexDirections = this.findAllDirections(vertex1)
     let paths = this.findPaths(vertex1, vertex2, null)
-    let shortestPathDirection
-    let shortestPathWeight
+
+    let shortestPath = {
+      direction: null,
+      weight: null
+    }
 
     paths.forEach(path => {
       if(path.weight){
-        if(typeof shortestPathWeight === 'number'){
-          if(path.weight < shortestPathWeight) {
-            shortestPathDirection = path.vertices
-            shortestPathWeight = path.weight
+        if(typeof shortestPath.weight === 'number'){
+          if(path.weight < shortestPath.weight) {
+            shortestPath.direction = path.vertices
+            shortestPath.weight = path.weight
           }
-        } else if(path.weight === shortestPathWeight){
-          shortestPathDirection = [shortestPathDirection, path.vertices]
+        } else if(path.weight === shortestPath.weight){
+          shortestPath.direction = [shortestPath.direction, path.vertices]
         }  else {
-          shortestPathDirection = path.vertices
-          shortestPathWeight = path.weight
+          shortestPath.direction = path.vertices
+          shortestPath.weight = path.weight
         }
       } else {
         let pathWeight = path.direction.weight
-        let pathString = path.direction.vertices
+        let pathString = [path.direction.vertices]
         path.path.forEach( path2 =>{
-          pathString = pathString + '->' + path2.vertices
+          pathString.push(path2.vertices)
           pathWeight += path2.weight
         })
-        if(pathWeight < shortestPathWeight){
-          shortestPathDirection = pathString
-          shortestPathWeight = path.direction.weight
-        } else if(path.direction.weight === shortestPathWeight){
-          shortestPathDirection = [shortestPathDirection, path.vertices]
+        if(pathWeight < shortestPath.weight){
+          shortestPath.direction = pathString
+          shortestPath.weight = path.direction.weight
+        } else if(pathWeight === shortestPath.weight){
+          shortestPath.direction = [shortestPath.direction, pathString]
         }
       }
     })
 
-    return shortestPathDirection
+    return shortestPath
   }
 
   removeDirection(vertex1, vertex2){
@@ -136,17 +139,3 @@ class DirectedGraph {
     return this.vertices.length
   }
 }
-
-const diGraph = new DirectedGraph()
-
-diGraph.addVertex('v1')
-// console.log(diGraph.vertices)             // adds a vertex to the graph.
-// console.log(diGraph.hasVertex('v1'))
-diGraph.addDirection('v1', 'v3', 4)              // returns true if the graph contains the vertex or false if not.
-diGraph.addDirection('v1', 'v2', 2)
-diGraph.addDirection('v2', 'v3', 2)  // adds a direction from 'v1' to 'v2' with a weight (number).
-// console.log(diGraph.hasDirection('v1', 'v2'))      // returns true if there's a direction from 'v1' to 'v2'.
-// console.log(diGraph.getDirectionWeight('v1', 'v2')) // returns direction weight between v1 & v2 or null if no direction exists.
-// diGraph.visit( 'v1', vertex => console.log(vertex)) // visit all the connected vertices in the graph starting with v1 and apply function on the reached vertex.
-console.log(diGraph.findShortestPath('v1', 'v3'))  // returns an array of all the shortest paths between two vertices based on the sum of weights.
-// diGraph.removeDirection('v1', 'v2')
