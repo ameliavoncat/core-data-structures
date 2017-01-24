75d3ef9cee157db94df80de33f86f93b170f6cb8
Amelia Clarke <ameliavoncat@gmail.com>
2017-01-24 09:37:24 -0800

    Stack

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,36 @@
 'use strict'
 
 export default class Stack {
-  // your code here
+  constructor(){
+    this.stack = []
+  }
+
+  stackPush(newOne){
+    this.stack.push(newOne)
+  }
+
+  stackPop(){
+    let newStack = []
+    let lastIndex = this.stack.length - 1
+    const result = this.stack[lastIndex]
+
+    for(let i=0; i<lastIndex; i++) newStack.push(this.stack[i])
+    this.stack = newStack
+
+    return result
+  }
+
+  stackPeek(){
+    let lastIndex = this.stack.length - 1
+
+    return this.stack[lastIndex]
+  }
+
+  isEmpty(){
+    return (this.stack.length === 0)
+  }
+
+  getLength(){
+    return this.stack.length
+  }
 }
